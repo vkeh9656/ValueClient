1c0c80d8f8db1af317e1f8c63afe0b42b48cb179
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-12-21 01:22:45 +0900

    초기 커밋.

---
+++ ValueClient.cpp
@@ -0,0 +1,54 @@
+﻿
+// ValueClient.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ValueClient.h"
+#include "ValueClientDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CValueClientApp
+
+BEGIN_MESSAGE_MAP(CValueClientApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CValueClientApp 생성
+
+CValueClientApp::CValueClientApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CValueClientApp 개체입니다.
+
+CValueClientApp theApp;
+
+
+// CValueClientApp 초기화
+
+BOOL CValueClientApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	if (!AfxSocketInit())
+	{
+		AfxMessageBox(IDP_SOCKETS_INIT_FAILED);
+		return FALSE;
+	}
+
+	CValueClientDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+	
+	return FALSE;
+}
+
---
+++ ValueClient.h
@@ -0,0 +1,32 @@
+﻿
+// ValueClient.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CValueClientApp:
+// 이 클래스의 구현에 대해서는 ValueClient.cpp을(를) 참조하세요.
+//
+
+class CValueClientApp : public CWinApp
+{
+public:
+	CValueClientApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CValueClientApp theApp;
